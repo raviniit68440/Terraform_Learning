ba02b5be5e4a92cc805a9db6c7e4063ae9bef14f
ravi gautam <ravigautam@ravis-MacBook-Air.local>
2026-07-06 19:48:27 +0530

    adding codes

--- Terraform/Practice/azurerm_variable/main.tf
+++ Terraform/Practice/azurerm_variable/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "rgs" {
 resource "azurerm_storage_account" "stgs" {
   for_each = var.resource_group
   name=each.value.storage_account_name
-  resource_group_name = each.key
+  resource_group_name = azurerm_resource_group.rgs[each.key].name
   location = each.value.location
   account_tier = each.value.account_tier
   account_replication_type = each.value.account_replication_type
